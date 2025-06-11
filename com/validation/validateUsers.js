e98ecdf2a136111ba62ba6bc7c35841878444dce
DanielGuillen88 <danielguillen85@gmail.com>
2025-06-11 22:58:52 +0200

    creamos validaciones constantes e implementamos en validaciones de usuarios / create constant validations and implement them in user validations

--- com/validation/validateUsers.js
+++ com/validation/validateUsers.js
@@ -1,30 +1,27 @@
 import { ContentError, MatchError, ValidationError } from '../errors/errors.js';
-
-const USERNAME_REGEX = /^[\w-]{3,20}$/; // Letras, números, guiones y guiones bajos, entre 3 y 20 caracteres
-const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,10}$/; // Al menos una letra y un número, entre 6 y 10 caracteres
-const ALPHANUMERIC_REGEX = /^[a-zA-Z0-9]+$/; // Solo letras y números, sin espacios ni caracteres especiales(codigo categoria acceso)
+import { VALIDATION } from './validationConstants.js';
 
 // username podra ser nombre, apellido o dni (Esto significa que combinaciones como "Juan Perez" no serán válidas. Ejemplos válidos serían "JuanPerez" o "Juan_Perez")
 export function validateUsername(username) { 
-    if (username === '' || username === null || username === undefined) {
+    if (!username) {
         throw new ContentError('❌ Nombre de usuario requerido ❌');
     }
-    if (typeof username !== 'string' || !USERNAME_REGEX.test(username)) {
+    if (typeof username !== 'string' || !VALIDATION.username.test(username)) {
         throw new ValidationError('❌ Nombre de usuario inválido, debe tener entre 3 y 20 caracteres y solo contener letras, números, guiones y guiones bajos ❌' );
     }
 }
 
 export function validatePassword(password) {
-    if (password === '' || password === null || password === undefined) {
+        if (!password) {
         throw new ContentError('❌ Contraseña de usuario requerida ❌');
     }
-    if (typeof password !== 'string' || !PASSWORD_REGEX.test(password)) {
+    if (typeof password !== 'string' || !VALIDATION.password.test(password)) {
         throw new ValidationError('❌ Contraseña inválida, debe tener entre 6 y 10 caracteres, al menos una letra y un número ❌' );
     }
 }
 
 export function validatePasswordRepeat(password, passwordRepeat) {
-    if (passwordRepeat === '' || passwordRepeat === null || passwordRepeat === undefined) {
+        if (!passwordRepeat) {
         throw new ContentError('❌ Para evitar errores, repita la contraseña ❌');
     }
     if (password !== passwordRepeat) {
@@ -33,10 +30,10 @@ export function validatePasswordRepeat(password, passwordRepeat) {
 }
 
 export function validateAccessCode(code) {
-    if (code === '' || code === null || code === undefined) {
+        if (!code) {
         throw new ContentError('❌ Código de acceso es requerido ❌');
     }
-    if (typeof code !== 'string' || !ALPHANUMERIC_REGEX.test(code)) {
+    if (typeof code !== 'string' || !VALIDATION.alphanumeric.test(code)) {
         throw new ValidationError('❌ Código de acceso inválido, prueba de nuevo o solicita uno nuevo al administrador ❌');
     }
 }
---
+++ com/validation/validationConstants.js
@@ -0,0 +1,19 @@
+export const VALIDATION = {
+    // Regex
+    // users
+    username: /^[\w-]{3,20}$/, // Letras, números, guiones y guiones bajos, entre 3 y 20 caracteres
+    password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,10}$/, // Al menos una letra y un número, entre 6 y 10 caracteres
+    alphanumeric: /^[a-zA-Z0-9]+$/, // Solo letras y números, sin espacios ni caracteres especiales(codigo categoria acceso)
+    // waste
+    code: /^\d{6}$/,
+    wasteName: /^[A-Z0-9ÁÉÍÓÚÜÑ\s]+$/,
+    weight: /^\d{1,4}$/,
+    month: /^(0[1-9]|1[0-2])$/,
+    year: /^\d{4}$/,
+  
+    // Opciones de arrays
+    containers: ['PALET', 'GRG', 'BIGBAG', 'B200', 'B<200'],
+    status: ['CORRECTO', 'ESTANCADO'], 
+    locationTypes: ['ALMACEN', 'SALIDA'],
+    warehouseAreas: ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'],
+  };
