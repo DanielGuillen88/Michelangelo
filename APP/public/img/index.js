e6574f6ad5010a2f079a8b42642e2d6f4222b739
DanielGuillen88 <danielguillen85@gmail.com>
2025-06-17 20:23:16 +0200

    carpeta de imágenes / images folder

---
+++ APP/public/img/index.js
@@ -0,0 +1,26 @@
+// img list
+import logo from './logo.png'
+import michelangelo64 from './michelangelo64.png'
+
+import palet from './palet.png'
+import grg from './grg.png'
+import bigbag from './bigbag.png'
+import b200 from './b200.png'
+import bm200 from './bm200.png'
+
+import vehicleBig from './VehicleBig.jpg'
+import vehicleMedium from './VehicleMedium.jpg'
+import vehicleSmall from './VehicleSmall.jpg'
+
+export {
+    logo,
+    michelangelo64,
+    palet,
+    grg,
+    bigbag,
+    b200, 
+    bm200,
+    vehicleSmall,
+    vehicleMedium,
+    vehicleBig,
+}
