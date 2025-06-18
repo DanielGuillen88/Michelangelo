02c7d2309ae03271370dff4dee4f8e559c421327
DanielGuillen88 <danielguillen85@gmail.com>
2025-06-19 00:11:55 +0200

    Implementar React Router con rutas basicas de autenticacion / Implement React Router with basic authentication routes

--- APP/src/App.jsx
+++ APP/src/App.jsx
@@ -1,12 +1,39 @@
-import './App.css'
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom'
 
-function App() {
+import HomePage from './pages/HomePage';
+import Dashboard from './pages/Dashboard';
+import LoginUser from './pages/LoginUser';
+import RegisterUser from './pages/RegisterUser';
+import AuthGuard from './components/AuthGuard';
+
+
+export default function App() {
   return (
-    <>
-      <h1>COWABUNGA!</h1>
-      
-    </>
+    <BrowserRouter>
+      <Container className="mt-5">
+        
+        <h1 className='text-center'>COWABUNGA!</h1> 
+
+        {/* Routes es el contenedor para todas tus definiciones de rutas */}
+        <Routes>
+          <Route path="/" element={< HomePage />} />
+
+          <Route path="/login" element={<LoginUser />} />
+          <Route path="/registro" element={<RegisterUser />} />
+
+          {/* Aqui pondremos las rutas PROTEGIDAS */}
+          <Route path="/dashboard" element={
+            <AuthGuard>
+              <Dashboard />
+            </AuthGuard>
+          } />
+
+          {/* futura ruta para manejar URLs no encontradas (404) */}
+          {/* <Route path="*" element={<h2>404 - Página No Encontrada</h2>} /> */}
+        </Routes>
+
+      </Container>
+    </BrowserRouter>
   )
 }
-
-export default App
