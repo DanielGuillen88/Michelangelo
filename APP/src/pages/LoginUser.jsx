01676feba25aea0ed78704cd512e18fbc0869380
DanielGuillen88 <danielguillen85@gmail.com>
2025-06-19 00:06:22 +0200

    renombrado login a LoginUser / renamed login a LoginUser

--- APP/src/pages/LoginUser.jsx
+++ APP/src/pages/LoginUser.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Container, Form, Button } from 'react-bootstrap';
 import InputField from '../components/InputField.jsx';
 import handleLoginSubmit from '../handlers/users/handleLoginSubmit.js';
@@ -11,14 +11,14 @@ export default function LoginUser() {
     const [message, setMessage] = useState('');
     const [messageType, setMessageType] = useState('');
 
-    // const navigate = useNavigate()
+    const navigate = useNavigate()
 
     return(
         <Container className="mt-5 d-flex justify-content-center">
         <div className="p-4 border rounded shadow-sm" style={{ maxWidth: '400px', width: '100%' }}>
           <h2 className="mb-4 text-center">Identificación de Usuario</h2>
 
-          <Form onSubmit={(event) => handleLoginSubmit(event, setMessage, setMessageType)}>
+          <Form onSubmit={(event) => handleLoginSubmit(event, setMessage, setMessageType, navigate)}>
             {/* Campo: Nombre de Usuario */}
             <InputField
                 label= "Nombre de Usuario" name="username"
@@ -32,9 +32,7 @@ export default function LoginUser() {
                 value={password} setValue={setPassword}
             />   
             {/* Botón de Envío */}
-            <Button variant="success" type="submit" className="w-100">
-              Registrarse
-            </Button>
+            <Button variant="success" type="submit" className="w-100">Iniciar Sesión</Button>
 
           </Form>
         {/* Componente Alert para mostrar mensajes al usuario */}
