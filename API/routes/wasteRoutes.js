deff0524a042636056cc46ade816f1cd70632d99
DanielGuillen88 <danielguillen85@gmail.com>
2025-08-12 19:03:13 +0200

    añadida ruta getWasteCustom / add endpoint getWasteCustom

--- API/routes/wasteRoutes.js
+++ API/routes/wasteRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import createWaste from "../handlers/waste/createWaste.js";
+import getWasteCustom from "../handlers/waste/getWasteCustom.js";
 import deleteWaste from "../handlers/waste/deleteWaste.js";
 
 const wasteRoutes = Router();
 
 wasteRoutes.post("/create", createWaste);
+
+wasteRoutes.get("/", getWasteCustom);
 
 wasteRoutes.delete("/delete/:id", deleteWaste);
 
