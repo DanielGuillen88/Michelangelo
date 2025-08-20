2cfdce104800ed4420ec515ce642a1ab9211f0d0
DanielGuillen88 <danielguillen85@gmail.com>
2025-08-20 20:17:28 +0200

    importar estilos de areas del almacen al modal con detalles del area / Import warehouse area styles into the modal with area details

--- APP/src/components/StoredWaste/StoredWasteAreaModal.jsx
+++ APP/src/components/StoredWaste/StoredWasteAreaModal.jsx
@@ -1,43 +1,44 @@
 import { Modal, Table } from 'react-bootstrap';
 
 export default function StoredWasteAreaModal({ show, handleClose, selectedArea }) {
+  
+  const modalColorClass = selectedArea?.colorClass ? selectedArea.colorClass : '';
+  const modalColorStyle = selectedArea?.colorStyle ? selectedArea.colorStyle : {};
+
+  
   return (
-    <Modal show={show} onHide={handleClose} scrollable>
-      <Modal.Header closeButton>
-        <Modal.Title>Detalles de Residuos en Área {selectedArea?.area}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {selectedArea && selectedArea.wasteItems.length > 0 ? (
-          <Table striped bordered hover responsive size="sm">
-            <thead>
-              <tr>
-                <th>Código</th>
-                <th>Nombre</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {selectedArea.wasteItems.map((wasteItem, index) => (
-                <tr key={index}>
-                  <td>{wasteItem.code}</td>
-                  <td>{wasteItem.name}</td>
-                  <td>{wasteItem.totalWeight.toFixed()}</td>
+    // <Modal className={`bg-light ${modalColorClass}`} style={{borderColor: modalColorStyle.borderColor}} show={show} onHide={handleClose} scrollable dialog>
+    <Modal show={show} onHide={handleClose} scrollable dialog>
+        
+        {/* <Modal.Header closeButton className={`bg-light ${modalColorClass}`} style={{borderColor: modalColorStyle.borderColor}}> */}
+        <Modal.Header className={`bg-light ${modalColorClass}`} style={{borderColor: modalColorStyle.borderColor}} closeButton>
+          <Modal.Title>Residuos en Área {selectedArea?.area}: {selectedArea?.totalWeight.toFixed()} Kg</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body className={`bg-light ${modalColorClass}`} style={{borderColor: modalColorStyle.borderColor}}>
+          {selectedArea && selectedArea.wasteItems.length > 0 ? (
+            <Table striped bordered hover responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Código</th>
+                  <th>Nombre</th>
+                  <th>Total</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-        ) : (
-          <p>No hay residuos almacenados en esta área.</p>
-        )}
-      </Modal.Body>
-      <Modal.Footer className="d-flex justify-content-end">
-        <span className="fw-bold fs-5">
-          Total: {selectedArea?.totalWeight.toFixed()} Kg
-        </span>
-        {/* <button className="btn btn-secondary" onClick={handleClose}>
-          Cerrar
-        </button> */}
-      </Modal.Footer>
+              </thead>
+              <tbody>
+                {selectedArea.wasteItems.map((wasteItem, index) => (
+                  <tr key={index}>
+                    <td>{wasteItem.code}</td>
+                    <td>{wasteItem.name}</td>
+                    <td>{wasteItem.totalWeight.toFixed()}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          ) : (
+            <p>No hay residuos almacenados en esta área.</p>
+          )}
+        </Modal.Body>
     </Modal>
   );
 };
